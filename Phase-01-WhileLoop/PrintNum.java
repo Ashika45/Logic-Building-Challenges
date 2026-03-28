674769d0a2e0ccd0f5df5f4bcfa83afaa09bc9ad
Ashika A <144346593+Ashika45@users.noreply.github.com>
2026-03-28 09:27:36 +0000

    feat: solve Phase 1 Question 1

---
+++ Phase-01-WhileLoop/PrintNum.java
@@ -0,0 +1,16 @@
+//Print all numbers from 1 to 10 using a loop. 
+
+public class PrintNum
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Printing numbers from 01 to 10");
+        int i=1;
+        while( i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+      
+    }
+}
