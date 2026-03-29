be18e6fb76631b0be35fe026eb252c774524fa1f
Ashika A <144346593+Ashika45@users.noreply.github.com>
2026-03-29 11:26:27 +0000

    question 2 added

---
+++ Phase-01-WhileLoop/PrintRev.java
@@ -0,0 +1,16 @@
+// Print numbers from 10 down to 1 in reverse order
+
+public class PrintRev
+{
+    public static void main (String [] args)
+    {
+        int i=10;
+        System.out.println("Reverse order from 10 to 01");
+
+        while(i>=1)
+        {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
